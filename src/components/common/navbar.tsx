69fdb110eeb54827735f1910080fc781a8a95ad7
Sławomir Zając <87309470+Mirac00@users.noreply.github.com>
2023-11-22 01:11:45 +0100

    sticky top navbar

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -2,13 +2,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
+import '../../style/navbarStyle.css';
 
 export {};
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
       <div className="container">
         <Link className="navbar-brand" to="/">Eko Project</Link>
         <button
